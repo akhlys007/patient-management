3e5d62d373926644625c64040871c725ff971151
Timothy Chelelgo <timmybarkuch007@gmail.com>
2025-06-05 15:05:47 +0300

    Created JwtUtil.java to generate JWT tokens for authentication validation Edited AuthController.java and AuthService.java to use the JwtUtil component for JWT token validation

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.util.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +12,17 @@ import java.util.Optional;
 public class AuthService {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
 
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
-        Optional<User> token = userService.findByEmail(loginRequestDTO.getEmail())
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
                 .filter(u -> passwordEncoder.matches(LoginRequestDTO.getPassword(),
                         u.getPassword()))
                 .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
